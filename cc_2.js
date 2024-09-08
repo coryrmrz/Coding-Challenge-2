4079c4e34e702ff04e587b96632c1075a34a0346
coryrmrz <coryramirez2018@gmail.com>
2024-09-07 22:25:32 -0400

    Initial logic for calculating tip using ternary operator

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+//Task 1: Calculate the Tip
+
+let bill = 125; // Amount of bill
+const tip = bill >= 100 ? bill * 0.2 : bill * 0.15; // If bill is higher than 100, then 20% tip, if not then 15% tip
+
+console.log(`Bill is the amount of $${bill}, the tip is the amount of $${tip}, the total amount is $${bill + tip}`); //Bill total explained
