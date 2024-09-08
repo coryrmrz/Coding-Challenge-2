a96b41798978e1b414b9bf080eb51f6e2c86c35b
coryrmrz <coryramirez2018@gmail.com>
2024-09-07 22:50:55 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -1,8 +1,18 @@
 //Task 1: Calculate the Tip
 
-let bill = 125; // Amount of bill
-const tip = bill >= 100 ? bill * 0.2 : bill * 0.15; // If bill is higher than 100, then 20% tip, if not then 15% tip
+let bill = 150; // Amount of bill
+const tip = bill >= 125 ? bill * 0.2 : bill * 0.15; // If bill is higher than 100, then 20% tip, if not then 15% tip
 
 //Task 2: Output Details
 
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill + tip}`); // Message displaying bill, tip, and total amount
+
+//Task 3: Create a Function
+
+function calculateTip(bill) {
+    return bill >= 125 ? bill * 0.2 : bill * 0.15; // Created function
+}
+
+let testBill = 100
+const testTip = calculateTip(testBill) 
+console.log(`The bill was $${testBill}, the tip was $${testTip}`) // Testing the function with a bill value of $100
