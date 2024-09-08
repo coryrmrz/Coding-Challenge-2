cadc1ba13a5e0306d7b899996dd4a3a4773f4a5d
coryrmrz <coryramirez2018@gmail.com>
2024-09-07 23:29:24 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc_2.js
+++ cc_2.js
@@ -10,9 +10,21 @@ console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is 
 //Task 3: Create a Function
 
 function calculateTip(bill) {
-    return bill >= 125 ? bill * 0.2 : bill * 0.15; // Created function
+    return bill >= 125 ? bill * 0.2 : bill * 0.15; // Created function to calculate tip
 }
 
-let testBill = 100
-const testTip = calculateTip(testBill) 
-console.log(`The bill was $${testBill}, the tip was $${testTip}`) // Testing the function with a bill value of $100
+let testBill = 100; //Test bill value
+const testTip = calculateTip(testBill); //Test tip function
+console.log(`The bill was $${testBill}, the tip was $${testTip}`); // Testing the function with a bill value of $100
+
+//Task 4: Utilize Arrays
+
+let bills1 = [275, 40, 430]; //Data Set 1 Array
+const tips1 = bills1.map(bill => calculateTip(bill)); //Data Set 1 Tips array
+const totals1 = bills1.map(bill => bills1 + tips1); // Data Set 1 Totals array
+console.log(`The total values are $${totals1}`); // Data Set 1 total value display
+
+let bills2 = [125, 555,44]; //Data Set 2 Array
+const tips2 = bills2.map(bill => calculateTip(bill)); //Data Set 2 Tips array
+const totals2 = bills2.map(bill => bills2 + tips2); // Data Set 2 Totals array
+console.log(`The total values are $${totals2}`); // Data Set 2 total value display
